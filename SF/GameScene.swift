810fa32f2315d0e57508acc752d3fbdcac6da81c
Serhii <fenixwhite@gmail.com>
2020-10-08 13:38:13 +0300

    Some changes in Game scene

--- SF/GameScene.swift
+++ SF/GameScene.swift
@@ -8,82 +8,89 @@
 
 import SpriteKit
 import GameplayKit
-
-class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+class GameScene: SKScene, SKPhysicsContactDelegate {
+        var Space: SKSpriteNode!
+        override func didMove(to view: SKView) {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+            physicsWorld.contactDelegate = self
+            physicsWorld.gravity = CGVector(dx: 0.0, dy: -0.8)
+        
+            //background
+            
+            let BackGround = SKSpriteNode(imageNamed: "BackGround")
+            BackGround.size = CGSize(width: UIScreen.main.bounds.width * 3.5, height: UIScreen.main.bounds.width * 3.5)
+            addChild(BackGround)
+            
+            // space ship
+            Space = SKSpriteNode (imageNamed: "Space")
+            Space.physicsBody = SKPhysicsBody(texture: Space.texture!, size: Space.size)
+            Space.physicsBody?.isDynamic = false
+            
+            addChild(Space)
+            
+            func distanceCalc(a: CGPoint, b: CGPoint) -> CGFloat {
+                return sqrt((b.x - a.x) + (b.y) + (b.y - a.y)*(b.y - a.y))
+            }
+            
+            //add space ship speed
+            func timeToTravelDistance(distance: CGFloat, speed: CGFloat) -> TimeInterval {
+                let time = distance / speed
+                return TimeInterval(time)
+            }
+            
+            let StoneCreate = SKAction.run {
+                let Stone = self.createStone()
+                self.addChild(Stone)
+            }
+            let StoneCreationDelay = SKAction.wait(forDuration: 1.0, withRange: 0.5)
+            let StoneSequenceAction = SKAction.sequence([StoneCreate, StoneCreationDelay])
+            _ = SKAction.repeatForever(StoneSequenceAction)
+            
+            run(StoneSequenceAction)
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        //move by finger touch
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            
+            if let touch = touches.first {
+                
+                let touchLocation = touch.location(in: self)
+                print(touchLocation)
+                
+                let moveAction = SKAction.move(to: touchLocation, duration: 1)
+                Space.run(moveAction)
+            }
+            
+        }
+        
+        //add stones
+    func createStone() -> SKSpriteNode {
+           let Stone = SKSpriteNode(imageNamed:"Stone");
+
+        Stone.position.x = CGFloat(frame.size.height)
+        Stone.position.y = frame.size.height / 2
+        
+        Stone.physicsBody = SKPhysicsBody(texture: Stone.texture!, size: Stone.size)
+        Stone.name = "Stone"
+        
+       
+           return Stone
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        override func update( _ currentTime: TimeInterval) {
+            let Stone = createStone()
+            addChild(Stone)
+        }
+        
+        //add physics for stones to make less nodes
+        override func didSimulatePhysics() {
+            enumerateChildNodes(withName: "Stone") { (Stone, stop) in
+                let hight = UIScreen.main.bounds.height
+                if Stone.position.y < -hight {
+                    Stone.removeFromParent()
+                }
+                
+            }
+        }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-}
+
